9260bdf1e1bb84d3ea634d416797221dbf36aa98
TheBloodthirster <13606074505@163.com>
2020-05-23 18:41:41 +0800

    20200523

--- MachineLearning/02-supervised-learning.py
+++ MachineLearning/02-supervised-learning.py
@@ -273,10 +273,11 @@ print("lr.intercept_: {}".format(lr.intercept_))
 print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, y_test))) 
 
+
 # %%
 # 我们来看一下 LinearRegression 在更 复杂的数据集上的表现，比如波士顿房价数据集。
 # 记住，这个数据集有 506 个样本和 105 个导出特征。首先，加载数据集并将其分为训练集和测试集。
-# 然后像前面一样构建线性回 归模型：
+# 然后像前面一样构建线性回归模型：
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -289,4 +290,95 @@ lr = LinearRegression().fit(X_train,y_train)
 print("Training set score: {:.2f}".format(lr.score(X_train, y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, y_test))) 
 
+# 训练集和测试集之间的性能差异是过拟合的明显标志
+# 因此我们应该试图找到一个可以控制复杂度的模型标准
+# 线性回归最常用的替代方法之一就是岭回归（ridge regression）
+
+
+# 3. 岭回归 岭回归也是一种用于回归的线性模型，因此它的预测公式与普通最小二乘法相同。
+# 但在岭回归中，对系数（w）的选择不仅要在训练数据上得到好的预测结果，而且还要拟合附加约束。
+# 我们还希望系数尽量小。换句话说，w 的所有元素都应接近于 0。
+# 直观上来看，这意味着每个特征对输出的影响应尽可能小（即斜率很小） ，同时仍给出很好的预测结果。
+# 这种约束是所谓正则化（regularization）的一个例子。
+# 正则化是指对模型做显式约束，以避免过拟合。岭回归用到的这种被称为 L2 正则化
+
+from sklearn.linear_model import Ridge
+ridge = Ridge().fit(X_train,y_train)
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test))) 
+
+# 可以看出，Ridge 在训练集上的分数要低于 LinearRegression，但在测试集上的分数更高。 
+# 这和我们的预期一致。线性回归对数据存在过拟合。Ridge 是一种约束更强的模型，所以更不容易过拟合。
+# 复杂度更小的模型意味着在训练集上的性能更差，但泛化性能更好。
+# 由于我们只对泛化性能感兴趣，所以应该选择 Ridge 模型而不是 LinearRegression 模型。 
+
+
+# Ridge 模型在模型的简单性（系数都接近于 0）与训练集性能之间做出权衡。
+# 简单性和训练 集性能二者对于模型的重要程度可以由用户通过设置 alpha 参数来指定。
+
+# 增大 alpha 会使得系数更加趋向于 0，从而降低训练集性能， 但可能会提高泛化性能。
+ridge10 = Ridge(alpha=10).fit(X_train,y_train)
+print("Training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge10.score(X_test, y_test))) 
+
+#减小 alpha 可以让系数受到的限制更小，即在图 2-1 中向右移动。
+# 对于非常小的 alpha 值， 系数几乎没有受到限制，我们得到一个与 LinearRegression 类似的模型
+ridge01 = Ridge(alpha=0.1).fit(X_train,y_train)
+print("Training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge01.score(X_test, y_test))) 
+
+plt.plot(ridge.coef_, 's', label="Ridge alpha=1")
+plt.plot(ridge10.coef_, '^', label="Ridge alpha=10")
+plt.plot(ridge01.coef_, 'v', label="Ridge alpha=0.1")
+plt.plot(lr.coef_, 'o', label="LinearRegression")
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.hlines(0, 0, len(lr.coef_))
+plt.ylim(-25, 25)
+plt.legend() 
+
+# 更大的 alpha 表示约束更强的模型，所以我们预计大 alpha 对应的 coef_ 元素比小 alpha 对应的 coef_ 元素要小。
+
+# 4. lasso 除了 Ridge，还有一种正则化的线性回归是 Lasso。
+# 与岭回归相同，使用 lasso 也是约束系 数使其接近于 0，但用到的方法不同，
+# 叫作 L1 正则化。 8L1 正则化的结果是，使用 lasso 时 某些系数刚好为 0。
+# 这说明某些特征被模型完全忽略。这可以看作是一种自动化的特征选择。
+# 某些系数刚好为 0，这样模型更容易解释，也可以呈现模型最重要的特征。 
+
+from sklearn.linear_model import Lasso
+
+lasso = Lasso().fit(X_train,y_train) 
+print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+# 如你所见，Lasso 在训练集与测试集上的表现都很差。
+# 这表示存在欠拟合，我们发现模型 只用到了 105 个特征中的 4 个。
+# 与 Ridge 类似，Lasso 也有一个正则化参数 alpha，可以控制系数趋向于 0 的强度。
+# 在上一个例子中，我们用的是默认值 alpha=1.0。为了降低欠拟 合，我们尝试减小 alpha。
+# 这么做的同时，我们还需要增加 max_iter 的值（运行迭代的最大次数）
+lasso001 = Lasso(alpha=0.01,max_iter=100000).fit(X_train,y_train) 
+print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+
+
+# alpha 值变小，我们可以拟合一个更复杂的模型，在训练集和测试集上的表现也更好。
+# 模型性能比使用 Ridge 时略好一点，而且我们只用到了 105 个特征中的 33 个。
+# 这样模型可能更容易理解。 但如果把alpha 设得太小，那么就会消除正则化的效果，并出现过拟合，
+# 得到与 LinearRegression 类似的结果：
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("Training set score: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("Number of features used: {}".format(np.sum(lasso00001.coef_ != 0))) 
+
+
+plt.plot(lasso.coef_, 's', label="Lasso alpha=1")
+plt.plot(lasso001.coef_, '^', label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, 'v', label="Lasso alpha=0.0001")
+plt.plot(ridge01.coef_, 'o', label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
 # %%
